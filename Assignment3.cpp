a1e11ac8768db525c7d8e03f6cc9c24a6501d670
Frederik <freddylohner@gmail.com>
2015-02-02 00:15:19 -0700

    Testing git within Sublime Text

--- Assignment3.cpp
+++ Assignment3.cpp
@@ -42,31 +42,31 @@ int printMenu(int choice) { //returns number depending on choice.
 	}
 }
 
-city *addCity(city *head, city *previous,	string cityName)
+// city *addCity(city *head, city *previous,	string cityName)
 
-/*Add	a	new	city	to	the	linked	list	between	the	city	*previous	and	the	city	that	
-follows	it	in	the	network.	The	name	of	the	new	city	is	in	the	argument	cityName.	
-Return	the	head	of	the	linked	list.	
-*/
+// /*Add	a	new	city	to	the	linked	list	between	the	city	*previous	and	the	city	that	
+// follows	it	in	the	network.	The	name	of	the	new	city	is	in	the	argument	cityName.	
+// Return	the	head	of	the	linked	list.	
+// */
 
-city *deleteCity(city *head, string	cityName)
+// city *deleteCity(city *head, string	cityName)
 
-/*Delete	the	city	in	the	linked	list	with	the	specified name.	Return	the	head	of	the	
-linked	list.	*/
+// /*Delete	the	city	in	the	linked	list	with	the	specified name.	Return	the	head	of	the	
+// linked	list.	*/
 
-void transmitMsg(city *head)
+// void transmitMsg(city *head)
 
-/*Open	the	file	messageLn.txt	and	transmit	the	message	between	all	cities	in	the	
-network	word	by	word.	A	word	needs	to	be	received	at	the	end	of	the	network	
-before	sending	the	next word.	Only	one	city	can	hold	the	message	at	a	time;	as	soon	
-as	it	is	passed	to	the	next	city,	it	needs	to	be	deleted	from	the	sender	city.	*/
+// Open	the	file	messageLn.txt	and	transmit	the	message	between	all	cities	in	the	
+// network	word	by	word.	A	word	needs	to	be	received	at	the	end	of	the	network	
+// before	sending	the	next word.	Only	one	city	can	hold	the	message	at	a	time;	as	soon	
+// as	it	is	passed	to	the	next	city,	it	needs	to	be	deleted	from	the	sender	city.	
 
-city *deleteEntireNetwork(city *head)
-/*This	function	deletes	all	cities	in	the	network	starting	at	the	head	city.	The	
-function	should	return	NULL	by	returning	the	next	pointer	of	the	last	city	deleted.
-void	printPath(city	*head)
-/*Go	through	each	city	in	the	network	starting	at	the	head	and	print	the	name	of	the	
-city.*/
+// city *deleteEntireNetwork(city *head)
+// /*This	function	deletes	all	cities	in	the	network	starting	at	the	head	city.	The	
+// function	should	return	NULL	by	returning	the	next	pointer	of	the	last	city	deleted.
+// void	printPath(city	*head)
+// /*Go	through	each	city	in	the	network	starting	at	the	head	and	print	the	name	of	the	
+// city.*/
 
 
 
