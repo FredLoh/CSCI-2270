c3fcc0f2743b20817b97dee4ef261b205e918be5
Frederik <freddylohner@gmail.com>
2015-02-04 23:50:15 -0700

    Finished work, fixed tons of bugs, got 100 on COG.

--- Assignment3.cpp
+++ Assignment3.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+
+/* Creates city struct, with name, message and two pointers next and prev */
 struct city {
   string name;
   string message;
@@ -12,19 +14,15 @@ struct city {
   city *prev;
 };
 
-// int printMenu(int choice) { //returns number depending on choice.
-
-// }
-
+/*Add	a	new	city	to	the	linked	list	between	the	city	*previous	and	the	city	that	
+follows	it	in	the	network.	The	name	of	the	new	city	is	in	the	argument	cityName.	
+Return	the	head	of	the	linked	list.	
+*/
 city *addCity(city *head, city *previous, string cityName)
 {	
-	city *cityAdder; //pointer to go through the linked list.
+	city *cityAdder = previous; //pointer to go through the linked list.
 	city *temp; //will hold the 
 	cityAdder = head;
-	while(cityAdder->prev != previous)
-	{
-		cityAdder = cityAdder->next;
-	}
 	cityAdder = new city;
 	temp = previous->next;
 	previous->next = cityAdder;
@@ -35,11 +33,9 @@ city *addCity(city *head, city *previous, string cityName)
 	return head;
 }
 
-// /*Add	a	new	city	to	the	linked	list	between	the	city	*previous	and	the	city	that	
-// follows	it	in	the	network.	The	name	of	the	new	city	is	in	the	argument	cityName.	
-// Return	the	head	of	the	linked	list.	
-// */
 
+// /*Delete	the	city	in	the	linked	list	with	the	specified name.	Return	the	head	of	the	
+// linked	list.	*/
 city *deleteCity(city *head, string cityName)
 {
 	city *cityDeleter;
@@ -58,8 +54,6 @@ city *deleteCity(city *head, string cityName)
 	return head;
 }
 
-// /*Delete	the	city	in	the	linked	list	with	the	specified name.	Return	the	head	of	the	
-// linked	list.	*/
 
 /* Open	the	file	messageLn.txt	and	transmit	the	message	between	all	cities	in	the	
 network	word	by	word.	A	word	needs	to	be	received	at	the	end	of	the	network	
@@ -72,46 +66,64 @@ void transmitMsg(city *head)
 	string word;
 	ifstream message;
 	message.open("messageIn.txt");
-	while(!message.eof())
+	if(messageTransmiter != NULL)
 	{
-		while(message >> word)
+		while(!message.eof())
 		{
-			while(messageTransmiter->next != NULL)
+			while(message >> word)
 			{
-				if(messageTransmiter == head)
+				while(messageTransmiter->next != NULL)
 				{
-				messageTransmiter->message = word;
-				cout << messageTransmiter->name << " received " << messageTransmiter->message << endl;
-				messageTransmiter = messageTransmiter->next;
+					if(messageTransmiter == head)
+					{
+					messageTransmiter->message = word;
+					cout << messageTransmiter->name << " received " << messageTransmiter->message << endl;
+					messageTransmiter = messageTransmiter->next;
+					}
+					else
+					{
+					messageTransmiter->message = word;
+					messageTransmiter->prev->message = "";
+					cout << messageTransmiter->name << " received " << messageTransmiter->message << endl;
+					// cout << messageTransmiter->prev->name << " received " << messageTransmiter->prev->message << endl; //Eraser checker.
+					messageTransmiter = messageTransmiter->next;
+					}
 				}
-				else
-				{
-				messageTransmiter->message = word;
-				messageTransmiter->prev->message = "";
-				cout << messageTransmiter->name << " received " << messageTransmiter->message << endl;
-				// cout << messageTransmiter->prev->name << " received " << messageTransmiter->prev->message << endl; //Eraser checker.
-				messageTransmiter = messageTransmiter->next;
-				}
+				messageTransmiter = head;
 			}
-			messageTransmiter = head;
 		}
 	}
 }
 
-// city *deleteEntireNetwork(city *head)
 // /*This	function	deletes	all	cities	in	the	network	starting	at	the	head	city.	The	
 // function	should	return	NULL	by	returning	the	next	pointer	of	the	last	city	deleted.
+city *deleteEntireNetwork(city *head)
+{	
+	city *networkClearer;
+	networkClearer = head;
+	while(networkClearer != NULL)
+	{
+		city *temp;
+		temp = networkClearer;
+		networkClearer = networkClearer->next;
+		delete temp;
+	}
+	return networkClearer;
 
+}
+
+// /*Go	through	each	city	in	the	network	starting	at	the	head	and	print	the	name	of	the	
+// city.*/
 void printPath(city *head)
 {
 	city *listPrinter;
 	listPrinter = head;
-	cout << "===Current Network Path===" << endl;
+	cout << "===CURRENT PATH===" << endl;
 			while(listPrinter != NULL)
 			{
 				if(listPrinter->name == "NULL")
 				{
-					cout << listPrinter->name << endl;
+					cout << " " << listPrinter->name << endl;
 					listPrinter = listPrinter->next;
 				}
 				else
@@ -123,33 +135,25 @@ void printPath(city *head)
 			cout << "=================" << endl;
 			listPrinter = head;
 }
-// /*Go	through	each	city	in	the	network	starting	at	the	head	and	print	the	name	of	the	
-// city.*/
-
-
-
-
-
 
 
 int main(int argc, char *argv[]) { //allows for command line arguments
 	std::string input;
 	while(input != "7")
 	{
-		cout << endl;
-		string cities[11] = {"Los Angeles", "Phoenix", "Denver", "Dallas", "St. Louis", "Chicago", "Atlanta", "Washington D.C.", "New York", "Boston", "NULL"};
+		string cities[11] = {"Los Angeles", "Phoenix", "Denver", "Dallas", "St. Louis", "Chicago", "Atlanta", "Washington, D.C.", "New York", "Boston", "NULL"};
 		int cityCounter;
 
 		city *head;
 		city *listPointer;
 
-		cout << "======Main Menu======" << endl;
+		cout << "======Main Menu=====" << endl;
 		cout << "1. Build Network" << endl;
 		cout << "2. Print Network Path" << endl;
 		cout << "3. Transmit Message Coast-To-Coast" << endl;
 		cout << "4. Add City" << endl;
 		cout << "5. Delete City" << endl;
-		cout << "6.Clear Network" << endl;
+		cout << "6. Clear Network" << endl;
 		cout << "7. Quit" << endl;
 		
 		
@@ -157,7 +161,6 @@ int main(int argc, char *argv[]) { //allows for command line arguments
 		
 		if(input == "1")
 		{
-			cout << "Build Network" << endl;
 			city *LosAngeles, *Phoenix, *Denver, *Dallas, *StLouis, *Chicago, *Atlanta, *DC, *NY, *Boston, *lastCity;
 			LosAngeles = new city; //Horrid coding practices but I was getting tired of getting seg faults so I did it by hand instead.
 			head = LosAngeles;
@@ -202,6 +205,26 @@ int main(int argc, char *argv[]) { //allows for command line arguments
 			}
 			listPointer = head;
 			cityCounter = 11;
+
+			city *listPrinter;
+			listPrinter = head;
+			cout << "===CURRENT PATH===" << endl;
+					while(listPrinter != NULL)
+					{
+						if(listPrinter->name == "NULL")
+						{
+							cout << " " << listPrinter->name << endl;
+							listPrinter = listPrinter->next;
+						}
+						else
+						{
+							cout << listPrinter->name << " -> ";
+							listPrinter = listPrinter->next;	
+						}	
+					}
+					cout << "=================" << endl;
+					listPrinter = head;
+
 		}
 		
 		else if(input == "2")
@@ -217,13 +240,15 @@ int main(int argc, char *argv[]) { //allows for command line arguments
 		
 		else if(input == "4")
 		{
-			cout << "Enter a city name:" << endl;
+			cout << "Enter a city name: " << endl;
 			string nameOfCity;
-			cin >> nameOfCity;
+			cin >> ws;
+			getline(cin,nameOfCity);
 			
-			cout << "Enter a previous city name:" << endl;
+			cout << "Enter a previous city name: " << endl;
 			string previousCity;
-			cin >> previousCity;
+			cin >> ws;
+			getline(cin,previousCity);
 
 			while(listPointer != NULL)
 			{
@@ -236,9 +261,9 @@ int main(int argc, char *argv[]) { //allows for command line arguments
 			listPointer=head;
 		}
 		
-		else if(input == "5")
+		else if(input == "5") //DELETE
 		{
-			cout << "Enter a city name to delete:" << endl;
+			cout << "Enter a city name: " << endl;
 			string cityToDelete;
 			cin >> cityToDelete;
 
@@ -255,12 +280,13 @@ int main(int argc, char *argv[]) { //allows for command line arguments
 		
 		else if(input == "6")
 		{
-			//
+			deleteEntireNetwork(head);
+			head = NULL;
 		}
 		
 		else if(input == "7")
 		{
-			//
+			cout << "Goodbye!" << endl;
 		}
 		
 		else
