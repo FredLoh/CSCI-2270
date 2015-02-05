b73b044992785abb3508f57041dcf7035816be79
Frederik Lohner <freddylohner@gmail.com>
2015-02-04 17:52:24 -0700

    Implemented Default Network Builder and printPath

--- Assignment3.cpp
+++ Assignment3.cpp
@@ -17,7 +17,7 @@ struct city {
 // }
 
 city *addCity(city *head, city *previous, string cityName)
-{
+{	
 	head = new city;
 	head->prev = previous;
 	head->name = cityName;
@@ -44,7 +44,28 @@ city *addCity(city *head, city *previous, string cityName)
 // city *deleteEntireNetwork(city *head)
 // /*This	function	deletes	all	cities	in	the	network	starting	at	the	head	city.	The	
 // function	should	return	NULL	by	returning	the	next	pointer	of	the	last	city	deleted.
-// void	printPath(city	*head)
+
+void printPath(city *head)
+{
+	city *listPrinter;
+	listPrinter = head;
+	cout << "===Current Network Path===" << endl;
+			while(listPrinter != NULL)
+			{
+				if(listPrinter->name == "NULL")
+				{
+					cout << listPrinter->name << endl;
+					listPrinter = listPrinter->next;
+				}
+				else
+				{
+					cout << listPrinter->name << " -> ";
+					listPrinter = listPrinter->next;	
+				}	
+			}
+			cout << "=================" << endl;
+			listPrinter = head;
+}
 // /*Go	through	each	city	in	the	network	starting	at	the	head	and	print	the	name	of	the	
 // city.*/
 
@@ -55,54 +76,102 @@ city *addCity(city *head, city *previous, string cityName)
 
 
 int main(int argc, char *argv[]) { //allows for command line arguments
-	
-
-
-	city *head;
-
-	cout << "======Main Menu======" << endl;
-	cout << "1. Build Network" << endl;
-	cout << "2. Print Network Path" << endl;
-	cout << "3. Transmit Message Coast-To-Coast" << endl;
-	cout << "4. Add City" << endl;
-	cout << "5. Delete City" << endl;
-	cout << "6.Clear Network" << endl;
-	cout << "7. Quit" << endl;
-	
 	std::string input;
-	// int intInput = stoi(input);
-	cin >> input;
-	if(input == "1")
+	while(input != "7")
 	{
-		cout << "Build Network" << endl;
-	}
-	else if(input == "2")
-	{
-		cout << "Print Network Path" << endl;
-	}
-	else if(input == "3")
+		cout << endl;
+		string cities[11] = {"Los Angeles", "Phoenix", "Denver", "Dallas", "St. Louis", "Chicago", "Atlanta", "Washington D.C.", "New York", "Boston", "NULL"};
+		int cityCounter;
 
-	{
-		//
-	}
-	else if(input == "4")
-	{
-		//
-	}
-	else if(input == "5")
-	{
-		//
-	}
-	else if(input == "6")
-	{
-		//
-	}
-	else if(input == "7")
-	{
-		//
-	}
-	else
-	{
-		cout << "Wrong choice detected, please choose again!" << endl;
+		city *head;
+		city *listPointer;
+
+		cout << "======Main Menu======" << endl;
+		cout << "1. Build Network" << endl;
+		cout << "2. Print Network Path" << endl;
+		cout << "3. Transmit Message Coast-To-Coast" << endl;
+		cout << "4. Add City" << endl;
+		cout << "5. Delete City" << endl;
+		cout << "6.Clear Network" << endl;
+		cout << "7. Quit" << endl;
+		
+		
+		cin >> input;
+		
+		if(input == "1")
+		{
+			cout << "Build Network" << endl;
+			city *LosAngeles, *Phoenix, *Denver, *Dallas, *StLouis, *Chicago, *Atlanta, *DC, *NY, *Boston, *lastCity;
+			LosAngeles = new city; //Horrid coding practices but I was getting tired of getting seg faults so I did it by hand instead.
+			head = LosAngeles;
+			Phoenix = new city;
+			Denver = new city;
+			Dallas = new city;
+			StLouis = new city;
+			Chicago = new city;
+			Atlanta = new city;
+			DC = new city;
+			NY = new city;
+			Boston = new city;
+			lastCity = new city;
+			listPointer = head;
+			
+			LosAngeles->next = Phoenix;
+			Phoenix->next = Denver;
+			Denver->next = Dallas;  
+			Dallas->next = StLouis;
+			StLouis->next = Chicago;
+			Chicago->next = Atlanta;
+			Atlanta->next = DC;
+			DC->next = NY;
+			NY->next = Boston;
+			Boston->next = lastCity;
+
+			
+			for(int i=0;i<11;i++)
+			{
+				listPointer->name = cities[i];
+				listPointer = listPointer->next;
+				
+			}
+			listPointer = head;
+			cityCounter = 11;
+		}
+		
+		else if(input == "2")
+		{
+			printPath(head);
+		}
+		
+		else if(input == "3")
+
+		{
+			//
+		}
+		
+		else if(input == "4")
+		{
+			//
+		}
+		
+		else if(input == "5")
+		{
+			//
+		}
+		
+		else if(input == "6")
+		{
+			//
+		}
+		
+		else if(input == "7")
+		{
+			//
+		}
+		
+		else
+		{
+			cout << "Wrong choice detected, please choose again!" << endl;
+		}
 	}
 }	
